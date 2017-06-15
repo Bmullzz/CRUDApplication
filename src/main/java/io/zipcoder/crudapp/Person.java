03c7ee93c6eab2d088a3025436ab5efe218cf6c6
Brian Mullin <brianmullin@zipcoder6.local>
2017-06-15 19:23:22 -0400

    added person class

---
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -0,0 +1,21 @@
+package io.zipcoder.crudapp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by brianmullin on 6/15/17.
+ */
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+
+
+}
