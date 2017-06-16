0146551ce68f11128d4bb73d861434a85d5ebe9f
Brian Mullin <brianmullin@zipcoder6.local>
2017-06-15 20:51:18 -0400

    finished PersonController

--- src/main/java/io/zipcoder/crudapp/controller/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/controller/PersonController.java
@@ -3,9 +3,7 @@ package io.zipcoder.crudapp.controller;
 import io.zipcoder.crudapp.person.Person;
 import io.zipcoder.crudapp.repo.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by brianmullin on 6/15/17.
@@ -22,6 +20,31 @@ public class PersonController { //handles requests
     public Iterable<Person> getAllPeople(){
         return personRepository.findAll();
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Person getPerson(@PathVariable("id") int id){
+        return personRepository.findOne(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deletePerson(@PathVariable("id") int id){
+        personRepository.delete(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public void updatePerson(@RequestBody Person person){
+        Person p = personRepository.findOne(person.getId());
+        p.setName(person.getName());
+        p.setAge(person.getAge());
+        personRepository.save(p);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void placePerson(@RequestBody Person person){
+        personRepository.save(person);
+    }
+
+
 
 
 
